9d829b4ce4a6298f3afc8a251eea4136c1c75442
Jan Jörg <jan.joerg.jj@gmail.com>
2024-01-14 18:44:24 +0100

    div

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-auto";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter(),
-    },
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
     preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({}),
+    },
 };
+
 export default config;
