335b1b9512d03a2c1b78183390e358c13ba17d86
Jan Jörg <jan.joerg.jj@gmail.com>
2024-01-20 02:01:59 +0100

    vorstand return order asc

--- src/routes/api/v1/main/getVorstand/+server.ts
+++ src/routes/api/v1/main/getVorstand/+server.ts
@@ -41,7 +41,7 @@ export async function GET({ request, locals }) {
                 },
                 orderBy: {
                     role: {
-                        id: "desc",
+                        id: "asc",
                     },
                 },
                 cacheStrategy: {
